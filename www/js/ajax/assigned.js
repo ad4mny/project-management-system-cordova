dfb6100de2d50a110a8b390984aac1b74cbd3fa1
adamz <58021562+ad4mny@users.noreply.github.com>
2022-01-05 07:03:14 +0800

    fixed assigns view

--- www/js/ajax/assigned.js
+++ www/js/ajax/assigned.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
                     var taskName = data[i].taskName.split(",");
                     var projectStartDate = data[i].projectStartDate.split(",");
                     var projectEndDate = data[i].projectEndDate.split(",");
+                    var projectName = data[i].projectName.split(",");
 
                     for (var j = 0; j < memberName.length; j++) {
                         memberList +=
@@ -34,10 +35,13 @@ $(document).ready(function () {
                         '<a href="#" class="text-decoration-none text-reset">' +
                         '                <div class="row m-1 py-3 px-1 bg-white shadow-sm" style="border-radius: 1em;">' +
                         '                    <div class="col-12">' +
-                        '                        <p class="mb-0">' + taskName[i] + '</p>' +
+                        '                        <p class="mb-0">' + taskName[0] + '</p>' +
                         '                    </div>' +
                         '                    <div class="col-12">' +
-                        '                        <small class="text-muted m-auto">' + projectStartDate[i] + ' <i class="fas fa-chevron-right fa-fw fa-xs"></i> ' + projectEndDate[i] + '</small>' +
+                        '                        <small class="m-auto">' + projectName[0] + '</small>' +
+                        '                    </div>' +
+                        '                    <div class="col-12">' +
+                        '                        <small class="text-muted m-auto">' + projectStartDate[0] + ' <i class="fas fa-chevron-right fa-fw fa-xs"></i> ' + projectEndDate[0] + '</small>' +
                         '                    </div>' +
                         '                    <div class="col-auto d-flex flex-row flex-wrap pt-3">' +
                         memberList +
@@ -49,10 +53,10 @@ $(document).ready(function () {
 
 
             } else {
-                $('#workspace-container').append(
+                $('#main-container').append(
                     '                <div class="row m-1 py-3 px-1 bg-white shadow-sm text-center" style="border-radius: 1em;">' +
                     '                    <div class="col-12">' +
-                    '                        <p class="mb-0 text-muted">No workspace available to show.</p>' +
+                    '                        <p class="mb-0 text-muted">No tasks available to show.</p>' +
                     '                    </div>' +
                     '                </div>'
                 );
